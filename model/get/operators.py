47b101def2590d42e61a45a7fed0047eea9d3fd0
arthur3000 <arthur3000.tw@gmail.com>
2024-08-08 21:28:31 +0800

    Modify - model/get/operators.py

--- model/get/operators.py
+++ model/get/operators.py
@@ -1,3 +1,11 @@
+from model.model.ResponseModel import MyResponse
 
 def getOperator(myDB):
-    return
+    # DB 指令
+    sql = "SELECT * FROM operators_info"
+    # 執行查詢
+    result = myDB.query(sql)
+    # 無查詢到資料
+    if len(result) == 0:
+        return MyResponse(status="error",message="查無資料")
+    return MyResponse(status="ok",data=result)
