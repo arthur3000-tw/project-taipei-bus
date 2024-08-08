c8353a06e89de2ee8891e9092d35cfa5c324a500
arthur3000 <arthur3000.tw@gmail.com>
2024-08-08 20:51:35 +0800

    Modify - model/model/ResponseModel.py

--- model/model/ResponseModel.py
+++ model/model/ResponseModel.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel, Json
+from enum import Enum
+
+class StatusEnum(str,Enum):
+    ok = "ok"
+    error = "error"
 
 class Response(BaseModel):
-    status: str
+    status: StatusEnum
     message: str = None
     data: Json = None
+
