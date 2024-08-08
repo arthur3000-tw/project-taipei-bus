3983ca46b9c34d7b17c353fd3bcaea4913114bdf
arthur3000 <arthur3000.tw@gmail.com>
2024-08-08 17:24:47 +0800

    Modify app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
+from model.db import DB
 
+# 實體化 fastapi
 app = FastAPI()
+# 設置靜態檔案路徑
+app.mount("/static",StaticFiles(directory='static', html=True))
+
+# DB 實體化
+myDB = DB.DB(host=os.environ.get("DB_HOST"),database="taipei_bus")
+myDB.initialize()
+
+# db instance 存放於 app.state 中
+app.state.db = myDB
