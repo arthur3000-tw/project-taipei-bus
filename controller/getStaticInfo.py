b134e997bb8e2b7d96527f1d09d9e22f59f9aca4
arthur3000 <arthur3000.tw@gmail.com>
2024-08-09 02:25:09 +0800

    Modify - controller/getStaticInfo.py

--- controller/getStaticInfo.py
+++ controller/getStaticInfo.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from model.get.operators import getOperators
+from model.get.routes import getRoutes
 from model.model.ResponseModel import MyResponse
 
 router = APIRouter()
@@ -9,16 +10,14 @@ router = APIRouter()
 async def get_operators(request: Request):
     myDB = request.app.state.db
     try:
-        result = getOperators(myDB)
-        return result
+        return getOperators(myDB)
     except:
         return JSONResponse(status_code=400,content=MyResponse(status="error",message="無法取得所有業者資訊").model_dump())
 
 @router.get("/routes/{operatorName}")
 async def get_routes_by_operator(request:Request,operatorName:str):
     myDB = request.app.state.db
-    try:
-        result = getRoutes(myDB,operatorName)
-        return result
+    try: 
+        return getRoutes(myDB,operatorName)
     except:
         return JSONResponse(status_code=400,content=MyResponse(status="error",message="無法取得業者所有路線資訊").model_dump())
