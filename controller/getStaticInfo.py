763a67917f658bebb7b7c1abfc2bb45ba5f2ad09
arthur3000 <arthur3000.tw@gmail.com>
2024-08-08 20:25:50 +0800

    Modify - controller/getStaticInfo.py

--- controller/getStaticInfo.py
+++ controller/getStaticInfo.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
+from model.get.operators import getOperator
 
 router = APIRouter()
 
 @router.get("/operators")
 async def get_operators(request: Request):
     myDB = request.app.state.db
-    return
+    try:
+        result = getOperator(myDB)
+        return result
+    except:
+        return JSONResponse(status_code=400)
