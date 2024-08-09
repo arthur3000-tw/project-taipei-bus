eef782276d857dda3f717a6cefe6495776dcb7fe
arthur3000 <arthur3000.tw@gmail.com>
2024-08-09 20:38:12 +0800

    Modify - controller/trip/getRoutePlatesDateAndTime.py

--- controller/trip/getRoutePlatesDateAndTime.py
+++ controller/trip/getRoutePlatesDateAndTime.py
@@ -10,6 +10,6 @@ router = APIRouter()
 async def get_route_plates_datetime(request:Request,route_name:str,start_date:date,end_date:date,start_time:time,end_time:time):
     myDB = request.app.state.db
     try:
-        return route_date_and_time(myDB,route_name,start_date,end_date,start_time,end_time)
+        return route_plates_date_and_time(myDB,route_name,start_date,end_date,start_time,end_time)
     except:
         return JSONResponse(status_code=400,content=MyResponse(status="error",message="無法取得資訊").model_dump())
