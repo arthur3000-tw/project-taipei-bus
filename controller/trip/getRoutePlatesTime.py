b9d1fcab697d80eb81121f226898ed50f047f900
arthur3000 <arthur3000.tw@gmail.com>
2024-08-09 20:54:42 +0800

    Modify - controller/trip/getRoutePlatesTime.py

---
+++ controller/trip/getRoutePlatesTime.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
+from datetime import time
+from model.model.ResponseModel import MyResponse
+from model.get.trip.routePlatesTime import route_plates_time
+
+router = APIRouter()
+
+@router.get("/Route/Plates/TripTime/")
+async def get_route_plates_time(request:Request,route_name:str,start_time:time,end_time:time):
+    myDB = request.app.state.db
+    try:
+        return route_plates_time(myDB,route_name,start_time,end_time)
+    except:
+        return JSONResponse(status_code=400,content=MyResponse(status="error",message="無法取得資訊").model_dump())
