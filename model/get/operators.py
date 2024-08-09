d61f9174a3a5a4d28755eed5c370b38bd40c0bb7
arthur3000 <arthur3000.tw@gmail.com>
2024-08-09 13:59:36 +0800

    Modify - model/get/routes.py opterators.py

--- model/get/operators.py
+++ model/get/operators.py
@@ -8,4 +8,5 @@ def getOperators(myDB):
     # 無查詢到資料
     if len(result) == 0:
         return MyResponse(status="error",message="查無資料")
+    # 回傳資料
     return MyResponse(status="ok",data=result)
--- model/get/routes.py
+++ model/get/routes.py
@@ -12,4 +12,5 @@ def getRoutes(myDB,operatorName):
     # 無查詢到資料
     if len(result) == 0:
         return MyResponse(status="error",message="查無資料")
+    # 回傳資料
     return MyResponse(status="ok",data=result)
