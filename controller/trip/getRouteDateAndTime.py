024a1a1c72caa79d0f46e21204c1ca0c300d5fd6
arthur3000 <arthur3000.tw@gmail.com>
2024-08-09 20:38:49 +0800

    Modify - controller/trip/getRouteDateAndTime.py

--- controller/trip/getRouteDateAndTime.py
+++ controller/trip/getRouteDateAndTime.py
@@ -7,7 +7,7 @@ from model.get.trip.routeDateAndTime import route_date_and_time
 router = APIRouter()
 
 @router.get("/Route/TripDateAndTime/")
-async def get_route_datetime(request:Request,route_name:str,start_date:date,end_date:date,start_time:time,end_time:time):
+async def get_route_date_and_time(request:Request,route_name:str,start_date:date,end_date:date,start_time:time,end_time:time):
     myDB = request.app.state.db
     try:
         return route_date_and_time(myDB,route_name,start_date,end_date,start_time,end_time)
