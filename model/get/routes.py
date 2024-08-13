ca15f82fe3bd8bbc11bb373b26eb7566df8ad2ba
arthur3000 <arthur3000.tw@gmail.com>
2024-08-13 20:38:07 +0800

    Modify - routes.py

--- model/get/routes.py
+++ model/get/routes.py
@@ -1,6 +1,7 @@
 from model.model.ResponseModel import MyResponse
 
-def getRoutes(myDB,operatorName):
+
+def getRoutes(myDB, operatorName):
     # DB 指令
     sql = "SELECT RouteName FROM routes_info \
            WHERE OperatorName=%s \
@@ -8,9 +9,9 @@ def getRoutes(myDB,operatorName):
            order by RouteName"
     val = (operatorName,)
     # 執行查詢
-    result = myDB.query(sql,val)
+    result = myDB.query(sql, val)
     # 無查詢到資料
     if len(result) == 0:
-        return MyResponse(status="error",message="查無資料")
+        return MyResponse(status="error", message="查無資料")
     # 回傳資料
-    return MyResponse(status="ok",data=result)
+    return MyResponse(status="ok", data=result)
