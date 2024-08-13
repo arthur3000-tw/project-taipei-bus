2c02c67472b48e53048f7827b8f710ac2f4c0809
arthur3000 <arthur3000.tw@gmail.com>
2024-08-13 20:45:57 +0800

    Modify - model/get/operatorRoutesCount.py

---
+++ model/get/operatorRoutesCount.py
@@ -0,0 +1,14 @@
+from model.model.ResponseModel import MyResponse
+
+
+def getOperators(myDB):
+    # DB 指令
+    sql = "SELECT OperatorName, COUNT(DISTINCT(RouteName)) FROM routes_info \
+           GROUP BY OperatorName"
+    # 執行查詢
+    result = myDB.query(sql)
+    # 無查詢到資料
+    if len(result) == 0:
+        return MyResponse(status="error", message="查無資料")
+    # 回傳資料
+    return MyResponse(status="ok", data=result)
