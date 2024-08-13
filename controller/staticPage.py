1da06f8a96e0a5c04c5b83186f00a2d95f88f0b2
arthur3000 <arthur3000.tw@gmail.com>
2024-08-13 16:49:28 +0800

    Modify - controller/staticPage.py

--- controller/staticPage.py
+++ controller/staticPage.py
@@ -3,6 +3,14 @@ from fastapi.responses import FileResponse
 
 router = APIRouter()
 
+
+# index
 @router.get("/", include_in_schema=False)
 async def index(request: Request):
     return FileResponse("./static/index.html", media_type="text/html")
+
+
+# analytics
+@router.get("/analytics", include_in_schema=False)
+async def analytics(request: Request):
+    return FileResponse("./static/analytics.html", media_type="text/html")
