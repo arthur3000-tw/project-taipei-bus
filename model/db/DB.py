8c681281a9219f3666aba5105e2c343169fc848e
arthur3000 <arthur3000.tw@gmail.com>
2024-08-14 18:36:27 +0800

    Modify for delete, truncate table by using insert()

--- model/db/DB.py
+++ model/db/DB.py
@@ -1,12 +1,14 @@
 from mysql.connector import pooling
 import os
 
+
 class DB:
     def __init__(self, host, database):
         self.host = host
         self.database = database
         self.cnxPool = None
     # 初始化資料庫，建立 pool 連線
+
     def initialize(self):
         # 資料庫訊息
         myDB = {
@@ -16,13 +18,15 @@ class DB:
             "database": self.database
         }
         # 建立連接池
-        self.cnxPool = pooling.MySQLConnectionPool(pool_name="myPool", pool_size=5, **myDB)
+        self.cnxPool = pooling.MySQLConnectionPool(
+            pool_name="myPool", pool_size=5, **myDB)
     # 向資料庫查詢
     # 輸入
     # sql: str      資料庫指令
     # val: str      查詢內容
     # 輸出
     # myResult      查詢結果
+
     def query(self, sql, val=None):
         # 從連接池取得連線
         cnx = self.cnxPool.get_connection()
@@ -41,7 +45,8 @@ class DB:
     # val: str      查詢內容
     # 輸出
     # myResult      輸出結果
-    def insert(self, sql, val):
+
+    def insert(self, sql, val=None):
         # 從連接池取得連線
         cnx = self.cnxPool.get_connection()
         # 進行操作
@@ -54,4 +59,3 @@ class DB:
         cnx.close()
         # 回傳結果
         return myResult
-
