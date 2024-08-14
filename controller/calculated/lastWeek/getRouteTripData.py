27edaeb4f8df9492ee8203404258ba878017afa4
arthur3000 <arthur3000.tw@gmail.com>
2024-08-14 21:13:21 +0800

    Modify - controller/calculated/lastWeek/getRouteTripData.py routeTripData.py

---
+++ controller/calculated/lastWeek/getRouteTripData.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
+from model.model.ResponseModel import MyResponse
+from model.get.calculated.lastWeek.routeTripData import route_trip_data
+
+router = APIRouter()
+
+
+@router.get("/LastWeek/RouteTrip")
+async def get_route_trip_data(request: Request, route_name: str):
+    myDB = request.app.state.db
+    try:
+        return route_trip_data(myDB, route_name)
+    except Exception:
+        return JSONResponse(status_code=400, content=MyResponse(status="error", message="無法取得資訊").model_dump())
