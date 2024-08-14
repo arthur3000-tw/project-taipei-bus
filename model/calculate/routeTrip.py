75a429b2b5d1eda0e7ab7ffc7de21263b593a844
arthur3000 <arthur3000.tw@gmail.com>
2024-08-14 20:23:39 +0800

    Modify - model/calculate/routeTrip.py

---
+++ model/calculate/routeTrip.py
@@ -0,0 +1,103 @@
+from model.db import DB
+from model.get.operators import getOperators
+from model.get.routes import getRoutes
+from datetime import datetime, timedelta
+import time
+
+start_time = time.time()
+
+# DB 實體化
+myDB = DB.DB(host="localhost", database="taipei_bus")
+# myDB = DB.DB(host=os.environ.get("DB_HOST"),database="taipei_bus")
+myDB.initialize()
+
+# 取得業者名稱
+operators = getOperators(myDB).data
+
+# 計算最近七天數據
+end_day = datetime.fromisoformat("2024-08-08")
+start_day = end_day + timedelta(days=-7)
+
+# 清空 table
+sql = "TRUNCATE TABLE routes_last_7_days_trip_data"
+myDB.insert(sql)
+
+for operator in operators:
+    result = getRoutes(myDB, operator["OperatorName"]).data
+    for route in result:
+        # print(route["RouteName"])
+        # DB 指令
+        sql = "select RouteName, SubRouteName, Direction, \
+               AVG(TripTime) as AVG, STDDEV(TripTime) as STD, COUNT(TripTime) as Count \
+               from route_time_records \
+               where RouteName=%s and \
+               GPSTime between %s and %s \
+               group by RouteName, SubRouteName, Direction;"
+        val = (route["RouteName"], start_day, end_day)
+        sql_results = myDB.query(sql, val)
+        # 若沒有資料
+        if len(sql_results) == 0:
+            continue
+        for sql_result in sql_results:
+            # 準備寫入 DB
+            operator_name = operator["OperatorName"]
+            route_name = sql_result["RouteName"]
+            subroute_name = sql_result["SubRouteName"]
+            direction = sql_result["Direction"]
+            avg_trip_time = sql_result["AVG"]
+            std_trip_time = sql_result["STD"]
+            count = sql_result["Count"]
+            # DB 指令
+            sql = "INSERT INTO routes_last_7_days_trip_data \
+                (OperatorName, RouteName, SubRouteName, Direction, AVG_TripTime, STD_TripTime, DataCount) \
+                VALUES (%s,%s,%s,%s,%s,%s,%s)"
+            val = (operator_name, route_name, subroute_name,
+                   direction, avg_trip_time, std_trip_time, count)
+            insert_result = myDB.insert(sql, val)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+start_time = time.time()
+
+# 計算最近三十天數據
+end_day = datetime.fromisoformat("2024-08-08")
+start_day = end_day + timedelta(days=-30)
+
+# 清空 table
+sql = "TRUNCATE TABLE routes_last_30_days_trip_data"
+myDB.insert(sql)
+
+for operator in operators:
+    result = getRoutes(myDB, operator["OperatorName"]).data
+    for route in result:
+        # print(route["RouteName"])
+        # DB 指令
+        sql = "select RouteName, SubRouteName, Direction, \
+               AVG(TripTime) as AVG, STDDEV(TripTime) as STD, COUNT(TripTime) as Count \
+               from route_time_records \
+               where RouteName=%s and \
+               GPSTime between %s and %s \
+               group by RouteName, SubRouteName, Direction;"
+        val = (route["RouteName"], start_day, end_day)
+        sql_results = myDB.query(sql, val)
+        # 若沒有資料
+        if len(sql_results) == 0:
+            continue
+        for sql_result in sql_results:
+            # 準備寫入 DB
+            operator_name = operator["OperatorName"]
+            route_name = sql_result["RouteName"]
+            subroute_name = sql_result["SubRouteName"]
+            direction = sql_result["Direction"]
+            avg_trip_time = sql_result["AVG"]
+            std_trip_time = sql_result["STD"]
+            count = sql_result["Count"]
+            # DB 指令
+            sql = "INSERT INTO routes_last_30_days_trip_data \
+                (OperatorName, RouteName, SubRouteName, Direction, AVG_TripTime, STD_TripTime, DataCount) \
+                VALUES (%s,%s,%s,%s,%s,%s,%s)"
+            val = (operator_name, route_name, subroute_name,
+                   direction, avg_trip_time, std_trip_time, count)
+            insert_result = myDB.insert(sql, val)
+
+print("--- %s seconds ---" % (time.time() - start_time))
