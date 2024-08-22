fc9fead04193b825ccf647bc994d4e205c2c3da8
arthur3000 <arthur3000.tw@gmail.com>
2024-08-22 17:37:02 +0800

    Modify - controller/staticPage.py

--- controller/staticPage.py
+++ controller/staticPage.py
@@ -14,3 +14,9 @@ async def index(request: Request):
 @router.get("/analytics", include_in_schema=False)
 async def analytics(request: Request):
     return FileResponse("./static/analytics.html", media_type="text/html")
+
+
+# realtime
+@router.get("/realtime", include_in_schema=False)
+async def realtime(request: Request):
+    return FileResponse("./static/realtime.html", media_type="text/html")
