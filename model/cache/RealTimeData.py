f3777ea21663fd5724ae1ad447906adccd71e881
arthur3000 <arthur3000.tw@gmail.com>
2024-08-23 15:10:24 +0800

    Modify - model/cache/RealTimeData.py

---
+++ model/cache/RealTimeData.py
@@ -0,0 +1,9 @@
+class RealTimeDataCache():
+    def __init__(self):
+        self.data = None
+
+    def getData(self):
+        return self.data
+
+    def setData(self, data):
+        self.data = data
