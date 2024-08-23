9175f72e95c75bc399e02ad3cb97f601696e882f
arthur3000 <arthur3000.tw@gmail.com>
2024-08-23 16:16:00 +0800

    Modify - routes.py for exporting RouteID, SubRouteID.

--- model/get/routes.py
+++ model/get/routes.py
@@ -3,13 +3,14 @@ from model.model.ResponseModel import MyResponse
 
 def getRoutes(myDB, operatorName):
     # DB 指令
-    sql = "SELECT RouteName FROM routes_info \
+    sql = "SELECT RouteName, RouteID, SubRouteID FROM routes_info \
            WHERE OperatorName=%s \
-           group by RouteName \
+           group by RouteName, RouteID, SubRouteID \
            order by RouteName"
     val = (operatorName,)
     # 執行查詢
     result = myDB.query(sql, val)
+    print(result)
     # 無查詢到資料
     if len(result) == 0:
         return MyResponse(status="error", message="查無資料")
