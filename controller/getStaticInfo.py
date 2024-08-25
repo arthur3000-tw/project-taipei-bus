f5c38562cf8fc2a2e30d608f74807f1f3c5a583f
arthur3000 <arthur3000.tw@gmail.com>
2024-08-25 18:21:47 +0800

    Modify - for adding new path

--- controller/getStaticInfo.py
+++ controller/getStaticInfo.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from model.get.operators import getOperators
+from model.get.routes import getRoutes
 from model.get.routesByOperator import getRoutesByOperator
 from model.get.operatorRoutesCount import getOperatorRoutesCount
 from model.model.ResponseModel import MyResponse
@@ -15,6 +16,15 @@ async def get_operators(request: Request):
         return getOperators(myDB)
     except Exception:
         return JSONResponse(status_code=400, content=MyResponse(status="error", message="無法取得所有業者資訊").model_dump())
+
+
+@router.get("/Routes")
+async def get_routes(request: Request):
+    myDB = request.app.state.db
+    try:
+        return getRoutes(myDB)
+    except Exception:
+        return JSONResponse(status_code=400, content=MyResponse(status="error", message="無法取得業者所有路線資訊").model_dump())
 
 
 @router.get("/Routes/{operatorName}")
