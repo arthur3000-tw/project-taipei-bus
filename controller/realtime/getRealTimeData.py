9c0c57e8ea59de86d2d103756bf6144d328312a8
arthur3000 <arthur3000.tw@gmail.com>
2024-08-28 03:24:35 +0800

    Modify - websocket broadcast data for specific route

--- controller/realtime/getRealTimeData.py
+++ controller/realtime/getRealTimeData.py
@@ -5,10 +5,10 @@ router = APIRouter()
 scheduler = AsyncIOScheduler()
 
 
-@router.websocket("/ws/realtime/{route_name}")
-async def get_real_time_data(websocket: WebSocket, route_name: str):
+@router.websocket("/ws/realtime/{route_id}")
+async def get_real_time_data(websocket: WebSocket, route_id: str):
     myWebSocket = websocket.app.state.websocket
-    await myWebSocket.connect(websocket, route_name)
+    await myWebSocket.connect(websocket, route_id)
     try:
         while True:
             data = await websocket.receive_json()
--- model/websocket/ConnectionManager.py
+++ model/websocket/ConnectionManager.py
@@ -6,18 +6,18 @@ from model.model.ResponseModel import MyResponse
 class ConnectionManager:
     def __init__(self, estimateTimeCache):
         self.activeConnections: List[WebSocket] = []
-        self.routeNames: dict = {}
+        self.routeIDs: dict = {}
         self.estimateTimeCache = estimateTimeCache
 
-    async def connect(self, websocket: WebSocket, routeName: str):
+    async def connect(self, websocket: WebSocket, routeID: str):
         await websocket.accept()
-        await websocket.send_json({"data": "連線成功", "routeName": routeName})
+        await websocket.send_json({"data": "連線成功", "routeName": routeID})
         self.activeConnections.append(websocket)
-        self.routeNames[websocket] = routeName
+        self.routeIDs[websocket] = routeID
 
     def disconnect(self, websocket: WebSocket):
         self.activeConnections.remove(websocket)
-        del self.routeNames[websocket]
+        del self.routeIDs[websocket]
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
@@ -28,4 +28,6 @@ class ConnectionManager:
 
     async def broadcast_json(self):
         for connection in self.activeConnections:
-            await connection.send_json(MyResponse(status="ok", data=[self.estimateTimeCache.data]).model_dump())
+            await connection.send_json(MyResponse(status="ok",
+                                                  data=[self.estimateTimeCache.data[int(self.routeIDs[connection])]])
+                                       .model_dump())
