1bb59c030b648f2b943d8e3ea83eec02acb5c4ae
arthur3000 <arthur3000.tw@gmail.com>
2024-08-27 15:54:21 +0800

    Modify - for realtime data schedualer

--- controller/realtime/getRealTimeData.py
+++ controller/realtime/getRealTimeData.py
@@ -1,9 +1,19 @@
 from fastapi import APIRouter, WebSocket
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 router = APIRouter()
+scheduler = AsyncIOScheduler()
 
 
 @router.websocket("/ws/realtime/{route_id}")
 async def get_real_time_data(websocket: WebSocket, route_id: str):
     myWebSocket = websocket.app.state.websocket
     await myWebSocket.connect(websocket, route_id)
+
+
+@router.on_event("startup")
+def start_scheduler():
+    scheduler.add_job(, "interval", seconds=5)
+    scheduler.start()
+
+
