ef80a6ea978b5f99821f0e3f81d6f18b8900ea73
arthur3000 <arthur3000.tw@gmail.com>
2024-08-28 11:51:12 +0800

    Modify - model/get/subrouteIDs.py

---
+++ model/get/subrouteIDs.py
@@ -0,0 +1,13 @@
+from model.model.ResponseModel import MyResponse
+
+
+def getSubroutesID(myDB):
+    # DB 指令
+    sql = "SELECT RouteID, SubRouteID FROM routes_info"
+    # 執行查詢
+    result = myDB.query(sql)
+    # 無查詢到資料
+    if len(result) == 0:
+        return MyResponse(status="error", message="查無資料")
+    # 回傳資料
+    return MyResponse(status="ok", data=result)
